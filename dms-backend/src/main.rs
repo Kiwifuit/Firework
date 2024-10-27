8abfa31d4b1e07edaa9b618b966c08d63f5d4aee
Kiwifuit <mahkiwi123@gmail.com>
2024-10-27 13:42:11 +0800

    feat: `ws`module for handling console and logs

--- dms-backend/src/main.rs
+++ dms-backend/src/main.rs
@@ -12,13 +12,27 @@ use tower_http::cors::CorsLayer;
 mod logger;
 mod routes;
 mod types;
+mod ws;
+
+// Route overview:
+// [POST, GET]          /api/servers
+// [POST]               /api/mod/search?sid=<server id>&query=<name>
+// [POST]               /api/modpack/search?sid=<server id>&query=<name>
+// [POST]               /api/datapack/search?sid=<server id>&query=<name>
+// [POST]               /api/dashboard?sid=<server id>
+// [POST, GET]          /api/options?sid=<server id>
+// [POST, GET, DELETE]  /api/players/[list,add,modify,delete]
+// [POST, GET]          /api/worlds/[upload,download]
+//
+// ws://localhost:3030/api/console
+// ws://localhost:3030/api/logs
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() -> anyhow::Result<()> {
   logger::init().context("while initializing logger")?;
 
   info!("Good morning!");
-  let ip_addr = var("DMS_HOST").unwrap_or(String::from("0.0.0.0:3030"));
+  let ip_addr = var("DMS_HOST").unwrap_or(String::from("localhost:3030"));
 
   let cors = CorsLayer::new()
     .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
