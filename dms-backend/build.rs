77e6714f9906a886e2dfd02f12f36bd6e3aa450d
Kiwifuit <mahkiwi123@gmail.com>
2024-10-27 15:49:06 +0800

    dev: cleaned build script

--- dms-backend/build.rs
+++ dms-backend/build.rs
@@ -1,16 +1,16 @@
-use std::fs::File;
-use std::io::Read;
-use std::path::PathBuf;
-use std::process::{Command, Stdio};
-use std::str::FromStr;
+// use std::fs::File;
+// use std::io::Read;
+// use std::path::PathBuf;
+// use std::process::{Command, Stdio};
+// use std::str::FromStr;
 
-use anyhow::Context;
-use lazy_static::lazy_static;
+// use anyhow::Context;
+// use lazy_static::lazy_static;
 
-lazy_static! {
-  static ref GIT_PATH: PathBuf =
-    PathBuf::from_str("../.git/").expect("expected path to be created");
-}
+// lazy_static! {
+//   static ref GIT_PATH: PathBuf =
+//     PathBuf::from_str("../.git/").expect("expected path to be created");
+// }
 
 static mut FEATURES: Vec<&'static str> = vec![];
 
@@ -27,20 +27,20 @@ fn main() -> anyhow::Result<()> {
   // Grab the HEAD commit hash and set it to
   // these 2 compile-time variables:
 
-  let bruh = get_git_sha_file()
-    .or_else(get_git_sha_command)
-    .context("while attempting to resolve git HEAD commit")
-    .context("this means that the build script was unable to get the HEAD commit");
+  //   let bruh = get_git_sha_file()
+  //     .or_else(get_git_sha_command)
+  //     .context("while attempting to resolve git HEAD commit")
+  //     .context("this means that the build script was unable to get the HEAD commit");
 
-  eprintln!("bruh: {:?}", bruh);
+  //   eprintln!("bruh: {:?}", bruh);
 
-  let git_sha = bruh?;
+  //   let git_sha = bruh?;
 
-  println!("cargo::rustc-env=GIT_SHA_LONG={}", git_sha);
-  println!(
-    "cargo::rustc-env=GIT_SHA_SHORT={}",
-    git_sha.get(..8).unwrap()
-  );
+  //   println!("cargo::rustc-env=GIT_SHA_LONG={}", git_sha);
+  //   println!(
+  //     "cargo::rustc-env=GIT_SHA_SHORT={}",
+  //     git_sha.get(..8).unwrap()
+  //   );
 
   // Feature listing
   detect_feature!("dms");
@@ -69,55 +69,55 @@ fn main() -> anyhow::Result<()> {
   Ok(())
 }
 
-fn get_git_sha_command(error: anyhow::Error) -> anyhow::Result<String> {
-  println!(
-    "cargo::warning=Failed to grab HEAD by file: {}. attempting git command",
-    error
-  );
+// fn get_git_sha_command(error: anyhow::Error) -> anyhow::Result<String> {
+//   println!(
+//     "cargo::warning=Failed to grab HEAD by file: {}. attempting git command",
+//     error
+//   );
 
-  let command = Command::new("git")
-    .args(vec!["rev-parse", "HEAD"])
-    .stdout(Stdio::piped())
-    .spawn()
-    .context("while trying to spawn `git rev-parse HEAD`")?;
-  let commit_hash = String::from_utf8(
-    command
-      .wait_with_output()
-      .context("while waiting for `git` output")?
-      .stdout,
-  )
-  .context("while parsing command output")?;
+//   let command = Command::new("git")
+//     .args(vec!["rev-parse", "HEAD"])
+//     .stdout(Stdio::piped())
+//     .spawn()
+//     .context("while trying to spawn `git rev-parse HEAD`")?;
+//   let commit_hash = String::from_utf8(
+//     command
+//       .wait_with_output()
+//       .context("while waiting for `git` output")?
+//       .stdout,
+//   )
+//   .context("while parsing command output")?;
 
-  eprintln!("commit hash empty: {}", commit_hash.is_empty());
+//   eprintln!("commit hash empty: {}", commit_hash.is_empty());
 
-  Ok(commit_hash)
-}
+//   Ok(commit_hash)
+// }
 
-fn get_git_sha_file() -> anyhow::Result<String> {
-  let head = GIT_PATH.join("head");
+// fn get_git_sha_file() -> anyhow::Result<String> {
+//   let head = GIT_PATH.join("head");
 
-  let mut ref_file =
-    File::open(&head).context(format!("while opening ref file: {}", head.display()))?;
-  let mut ref_content = String::new();
+//   let mut ref_file =
+//     File::open(&head).context(format!("while opening ref file: {}", head.display()))?;
+//   let mut ref_content = String::new();
 
-  ref_file
-    .read_to_string(&mut ref_content)
-    .context(format!("while reading ref file: {}", head.display()))?;
+//   ref_file
+//     .read_to_string(&mut ref_content)
+//     .context(format!("while reading ref file: {}", head.display()))?;
 
-  ref_content.truncate(ref_content.len() - 1);
+//   ref_content.truncate(ref_content.len() - 1);
 
-  let head_ref = ref_content.strip_prefix("ref: ").unwrap();
-  let git_head_commit = GIT_PATH.join(head_ref);
-  let mut git_head = File::open(&git_head_commit).context(format!(
-    "while opening git head file: {:?}",
-    git_head_commit.display()
-  ))?;
-  let mut git_sha = String::new();
+//   let head_ref = ref_content.strip_prefix("ref: ").unwrap();
+//   let git_head_commit = GIT_PATH.join(head_ref);
+//   let mut git_head = File::open(&git_head_commit).context(format!(
+//     "while opening git head file: {:?}",
+//     git_head_commit.display()
+//   ))?;
+//   let mut git_sha = String::new();
 
-  git_head.read_to_string(&mut git_sha).context(format!(
-    "while reading git head file: {:?}",
-    git_head_commit.display()
-  ))?;
+//   git_head.read_to_string(&mut git_sha).context(format!(
+//     "while reading git head file: {:?}",
+//     git_head_commit.display()
+//   ))?;
 
-  Ok(git_sha)
-}
+//   Ok(git_sha)
+// }
