bb2c297024dd27c23d778d10fd91e5470ab6770f
Kiwifuit <mahkiwi123@gmail.com>
2024-12-18 21:49:31 +0800

    deps: removed dependencies that i will merge into elytra

--- dms-backend/src/routes/versions.rs
+++ dms-backend/src/routes/versions.rs
@@ -48,7 +48,6 @@ pub async fn get_loaders() -> Json<Vec<String>> {
   )
 }
 
-#[cfg_attr(debug_assertions, axum::debug_handler)]
 pub async fn get_loader_version(
   Path(loader): Path<String>,
 ) -> Result<Json<Vec<String>>, VersionError> {
